43d18b3220499d4e8e4c9dd9f874f02f16d6320f
Renan Pupin <renan.pupin@gmail.com>
2024-11-02 00:04:04 +0100

    ops: try middleware

---
+++ lambda-config.js
@@ -0,0 +1,8 @@
+const {SimpleSpanProcessor} = require('@opentelemetry/sdk-trace-base')
+const {OTLPTraceExporter} = require('@opentelemetry/exporter-trace-otlp-proto')
+
+global.configureTracerProvider = tracerProvider => {
+    console.log('Configuring tracer provider with Simple Span Processor')
+    const spanProcessor = new SimpleSpanProcessor(new OTLPTraceExporter())
+    tracerProvider.addSpanProcessor(spanProcessor)
+}
--- sst.config.ts
+++ sst.config.ts
@@ -73,44 +73,47 @@ export default $config({
     app(input) {
         // console.log('input =====>>>> ', input)
         return {
-            name: "sst-v3-prisma",
-            removal: input?.stage === "production" ? "retain" : "remove",
-            home: "aws",
+            name: 'sst-v3-prisma',
+            removal: input?.stage === 'production' ? 'retain' : 'remove',
+            home: 'aws',
             // providers: {
             //     aws: { region: "sa-east-1" }
             // },
-            architecture: "arm64"
-        };
+            architecture: 'arm64'
+        }
     },
     console: {
-      autodeploy: {
-        target(event) {
-          // if (event.type === "branch" && event.branch === "staging") return;
-          if (event.type === "branch" && event.branch === "main" && event.action === "pushed") {
-            return {
-              stage: "production",
-              // stage: event.branch
-              //     .replace(/[^a-zA-Z0-9-]/g, "-")
-              //     .replace(/-+/g, "-")
-              //     .replace(/^-/g, "")
-              //     .replace(/-$/g, ""),
-              runner: {
-                  engine: "codebuild",
-                  compute: "small", //large
-                  architecture: "arm64", //arm64
-                  // timeout: "1 hour"
-              }
-            };
-          }
+        autodeploy: {
+            target(event) {
+                // if (event.type === "branch" && event.branch === "staging") return;
+                if (
+                    event.type === 'branch' &&
+                    event.branch === 'main' &&
+                    event.action === 'pushed'
+                ) {
+                    return {
+                        stage: 'production',
+                        // stage: event.branch
+                        //     .replace(/[^a-zA-Z0-9-]/g, "-")
+                        //     .replace(/-+/g, "-")
+                        //     .replace(/^-/g, "")
+                        //     .replace(/-$/g, ""),
+                        runner: {
+                            engine: 'codebuild',
+                            compute: 'small', //large
+                            architecture: 'arm64' //arm64
+                            // timeout: "1 hour"
+                        }
+                    }
+                }
 
-          if (event.type === "pull_request") {
-            return { stage: `pr-${event.number}` };
-          }
+                if (event.type === 'pull_request') {
+                    return {stage: `pr-${event.number}`}
+                }
+            }
         }
-      }
     },
     async run() {
-
         // console.log('process.env =====>>>>', process.env)
         const stage = process.env.SST_STAGE as string
 
@@ -119,14 +122,26 @@ export default $config({
 
         $transform(sst.aws.Function, (args, opts) => {
             // console.log('=====>>>>', args, opts)
-          // args.runtime = "nodejs14.x";
-            args.architecture = "arm64";
-          args.environment = {
-              DATABASE_URL: process.env.DATABASE_URL as string,
-              // ACCOUNT: aws.getCallerIdentityOutput({}).accountId,
-              // REGION: aws.getRegionOutput().name
-          };
-        });
+            // args.runtime = "nodejs14.x";
+            args.architecture = 'arm64'
+            args.environment = {
+                DATABASE_URL: process.env.DATABASE_URL as string,
+                // ACCOUNT: aws.getCallerIdentityOutput({}).accountId,
+                // REGION: aws.getRegionOutput().name
+                AWS_LAMBDA_EXEC_WRAPPER: '/opt/otel-handler',
+                NODE_OPTIONS: '--require ./lambda-config.js',
+                OTEL_SERVICE_NAME: 'sst-v3-prisma',
+                OTEL_EXPORTER_OTLP_ENDPOINT: 'https://rxcto.middleware.io',
+                OTEL_RESOURCE_ATTRIBUTES: 'mw.account_key=mlqxrmrfoblijrpfilgnqbotrfdmnckfyqaw',
+                OTEL_LAMBDA_DISABLE_AWS_CONTEXT_PROPAGATION: 'true',
+                OTEL_PROPAGATORS: 'tracecontext'
+            }
+
+            args.layers = [
+                'arn:aws:lambda:sa-east-1:184161586896:layer:opentelemetry-collector-amd64-0_11_0:1',
+                'arn:aws:lambda:sa-east-1:184161586896:layer:opentelemetry-nodejs-0_9_0:4'
+            ]
+        })
 
         new sst.x.DevCommand('Prisma Studio', {
             environment: {DATABASE_URL: process.env.DATABASE_URL as string},
@@ -141,20 +156,21 @@ export default $config({
         //     // az: 1 //no lb
         // });
 
-        const api = new sst.aws.Function("MyApi", {
+        const api = new sst.aws.Function('MyApi', {
             // vpc,
             url: true,
             // For ARM
             // architecture: "arm64",
-            handler: "index.handler",
+            handler: 'index.handler',
             // environment: {
             //     DATABASE_URL: process.env.DATABASE_URL as string,
             // }
-            copyFiles: process.env.IS_LOCAL !== 'true' ? [{ from: "node_modules/.prisma/client/" }] : [],
-        });
+            copyFiles:
+                process.env.IS_LOCAL !== 'true' ? [{from: 'node_modules/.prisma/client/'}] : []
+        })
 
         return {
-            api: api.url,
-        };
-    },
-});
+            api: api.url
+        }
+    }
+})
