482b649cf3a7123d41e49091ab7a4064f577924c
Renan Pupin <renan.pupin@gmail.com>
2024-11-02 02:00:23 +0100

    test

--- index.ts
+++ index.ts
@@ -1,12 +1,36 @@
-import {prisma} from "./prisma";
+const {trace} = require('@opentelemetry/api')
 
-export async function handler() {
-    const users = await prisma.user.findMany({
-        take: 6
-    })
-    console.log('users', users)
-    return {
-        statusCode: 200,
-        body: JSON.stringify({users}),
-    };
+import {prisma} from './prisma'
+
+const tracer = trace.getTracer('sst-v3-prisma', '1.0.0')
+
+export async function handler(event: any, context: any) {
+    const span = tracer.startSpan('lambdaHandler')
+
+    try {
+        span.setAttribute('event', JSON.stringify(event))
+
+        const users = await prisma.user.findMany({
+            take: 6
+        })
+
+        console.log('users', users)
+
+        span.addEvent('Lambda execution completed')
+
+        console.log('testing...')
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({users})
+        }
+    } catch (error: any) {
+        span.recordException(error)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({error: error.message})
+        }
+    } finally {
+        span.end()
+    }
 }
