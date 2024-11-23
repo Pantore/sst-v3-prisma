61eb4089a13337c60b5eeef39df4c8133b0e8108
Renan Pupin <renan.pupin@gmail.com>
2024-11-23 19:55:23 +0100

    disable prisma instrumentation

--- lambda-config.js
+++ lambda-config.js
@@ -1,7 +1,7 @@
 const {SimpleSpanProcessor} = require('@opentelemetry/sdk-trace-base')
 const {OTLPTraceExporter} = require('@opentelemetry/exporter-trace-otlp-proto')
 const {registerInstrumentations} = require('@opentelemetry/instrumentation')
-const {PrismaInstrumentation} = require('@prisma/instrumentation')
+// const {PrismaInstrumentation} = require('@prisma/instrumentation')
 const {MySQLInstrumentation} = require('@opentelemetry/instrumentation-mysql')
 global.configureTracerProvider = tracerProvider => {
     console.log('Configuring tracer provider with Simple Span Processor')
@@ -9,5 +9,6 @@ global.configureTracerProvider = tracerProvider => {
     tracerProvider.addSpanProcessor(spanProcessor)
 }
 registerInstrumentations({
-    instrumentations: [new PrismaInstrumentation(), new MySQLInstrumentation()]
+    // instrumentations: [new PrismaInstrumentation(), new MySQLInstrumentation()]
+    instrumentations: [new MySQLInstrumentation()]
 })
