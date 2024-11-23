9c0d39c3b34d942daac92fb35015fd7997320557
Renan Pupin <renan.pupin@gmail.com>
2024-11-23 19:26:46 +0100

    instrumentation

--- lambda-config.js
+++ lambda-config.js
@@ -1,8 +1,8 @@
 const {SimpleSpanProcessor} = require('@opentelemetry/sdk-trace-base')
 const {OTLPTraceExporter} = require('@opentelemetry/exporter-trace-otlp-proto')
-const {registerInstrumentations} = require('@opentelemetry/instrumentation')
-const {PrismaInstrumentation} = require('@prisma/instrumentation')
-const {MySQLInstrumentation} = require('@opentelemetry/instrumentation-mysql')
+// const {registerInstrumentations} = require('@opentelemetry/instrumentation')
+// const {PrismaInstrumentation} = require('@prisma/instrumentation')
+// const {MySQLInstrumentation} = require('@opentelemetry/instrumentation-mysql')
 global.configureTracerProvider = tracerProvider => {
     console.log('Configuring tracer provider with Simple Span Processor')
     const spanProcessor = new SimpleSpanProcessor(new OTLPTraceExporter())
